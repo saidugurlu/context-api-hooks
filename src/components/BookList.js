f7275a0d35b5f2971cb850f0ccfc7ac537eb68fa
saidugurlu <ugurlumsaid@gmx.de>
2022-05-14 11:30:00 +0200

    minimized

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+    render() {
+        return (
+            <div>
+                Booklist
+            </div>
+        );
+    }
+}
+
+export default Booklist;
