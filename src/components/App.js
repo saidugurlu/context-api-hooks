4ab7400284ea80c93032391cb1936f72e697aa36
saidugurlu <ugurlumsaid@gmx.de>
2022-05-14 19:21:22 +0200

    context finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import "../App.css";
 import React, { Component } from "react";
 import BookList from "./BookList";
+import BookContextProvider from '../contexts/BookContext'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BookList />
+        <BookContextProvider>
+        <BookList /></BookContextProvider>
       </div>
     );
   }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,29 +1,32 @@
 import React, { Component } from "react";
 import Book from "./Book";
 import "./BookList.css";
-import BookContext from "../contexts/BookContext";
+import { BookContext } from "../contexts/BookContext";
 
 class Booklist extends Component {
-  static contextType = BookContext;
   render() {
-    const books = this.context;
-
     return (
-      <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">Kitaplik</h2>
-            <h3 className="section-subheading text-muted">
-              Taktuka taktuka tutukaka kakakuklata.
-            </h3>
-          </div>
-          <div className="row">
-            {books.map((book, i) => {
-              return <Book book={book} key={i} />;
-            })}
-          </div>
-        </div>
-      </section>
+      <BookContext.Consumer>
+        {(value) => {
+          return (
+            <section className="page-section bg-light" id="portfolio">
+              <div className="container">
+                <div className="text-center">
+                  <h2 className="section-heading text-uppercase">Kitaplik</h2>
+                  <h3 className="section-subheading text-muted">
+                    Taktuka taktuka tutukaka kakakuklata.
+                  </h3>
+                </div>
+                <div className="row">
+                  {value.books.map((book, i) => {
+                    return <Book book={book} key={i} />;
+                  })}
+                </div>
+              </div>
+            </section>
+          );
+        }}
+      </BookContext.Consumer>
     );
   }
 }
